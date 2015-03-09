40dee1617c834552cea8537b14c6813704f7be36
guptasonal <guptasonal1234@gmail.com>
2015-03-09 17:57:25 +0530

    Added file to click pics

---
+++ click.py
@@ -0,0 +1,12 @@
+def click(image_name="photo.bmp"):
+	import pygame.camera
+	pygame.camera.init()
+	cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
+	cam.start()
+	img = cam.get_image()
+	import pygame.image
+	pygame.image.save(img, image_name)
+	pygame.camera.quit()
+	cam.stop()
+
+click()
